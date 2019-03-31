2e2f95e4f4c29876bcfdf37ab5ad9f0795d0eb5c
cabiapi <robert.druska@gmail.com>
2019-03-31 12:06:46 +0200

    last change

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC1VIqSzswjoYFA7aAsOhXCelPiksMuUpM",
+    authDomain: "medicalapp-b851d.firebaseapp.com",
+    databaseURL: "https://medicalapp-b851d.firebaseio.com",
+    projectId: "medicalapp-b851d",
+    storageBucket: "medicalapp-b851d.appspot.com",
+    messagingSenderId: "185776824876"
+  }
 };
